1b890896dd1bfd684543e93219e5ac0f54acb3fe
YaelAbbo <email@example.com>
2026-03-07 19:50:18 +0200

    add main layout with navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
 import { OAuthCallbackPage } from './pages/OAuthCallbackPage';
+import { MainLayout } from './layouts/MainLayout';
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/oauth-callback' element={<OAuthCallbackPage />} />
+
+        <Route path='/home' element={<MainLayout>Home screen coming soon... </MainLayout>} />
+        <Route path='/create' element={<MainLayout>Create post screen coming soon... </MainLayout>} />
+        <Route path='/profile' element={<MainLayout>Profile screen coming soon... </MainLayout>} />
       </Routes>
     </BrowserRouter>
   );
