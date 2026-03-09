81b4eccf7c275bb0b660b0c486583a782130ff9b
yishaic11 <yishai.chen28@gmail.com>
2026-03-09 19:54:21 +0200

    Added home page with logic

--- frontend/src/api/users.api.ts
+++ frontend/src/api/users.api.ts
@@ -2,6 +2,12 @@ import api from './axios';
 import type { UserResponseDto } from '../types/auth.types';
 
 export const usersApi = {
+  getById: async (id: string): Promise<UserResponseDto> => {
+    const { data } = await api.get<UserResponseDto>(`/users/${id}`);
+
+    return data;
+  },
+
   update: async (id: string, payload: { username?: string; photo?: File }): Promise<UserResponseDto> => {
     const formData = new FormData();
 
