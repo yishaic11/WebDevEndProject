7eceb92a0c4e2219cf814610ada68d40f410ecf1
yishaic11 <yishai.chen28@gmail.com>
2026-03-11 17:15:52 +0200

    Updated prod -> production as expected

--- backend/utils/url.ts
+++ backend/utils/url.ts
@@ -1,5 +1,5 @@
 export const getBaseUrl = (): string => {
-  const prefix = process.env.NODE_ENV === 'prod' ? 'https' : 'http'
+  const prefix = process.env.NODE_ENV === 'production' ? 'https' : 'http'
   const domain = process.env.DOMAIN_BASE || 'localhost';
   const port = process.env.PORT || '3000';
   return `${prefix}://${domain}:${port}/`;
